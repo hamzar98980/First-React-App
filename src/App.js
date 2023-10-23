301cb92ba711d52b2ba7034fcd3f4be71e4f8ae4
hamzar98980 <hamzar98980@gmail.com>
2023-10-24 02:37:17 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  let [count, stateCount] = useState(0);
+
+  function counterfun() {
+    stateCount((prev) => prev + 1);
+  }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h3>Counter : {count}</h3>
+      <br />
+      <button onClick={counterfun}> Incremnet</button>
     </div>
+
   );
 }
 
